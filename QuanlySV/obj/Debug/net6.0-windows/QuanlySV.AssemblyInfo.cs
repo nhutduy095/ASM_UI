fc05a0d86d50b0389e794811437953dd617cf3b7
Admin01 <duy.nguyen@thlone.vn>
2023-11-27 22:13:41 +0700

    ..

--- QuanlySV/obj/Debug/net6.0-windows/QuanlySV.AssemblyInfo.cs
+++ QuanlySV/obj/Debug/net6.0-windows/QuanlySV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanlySV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c78250bd42b719df9c82eabadfce0ae2230b8f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanlySV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanlySV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
