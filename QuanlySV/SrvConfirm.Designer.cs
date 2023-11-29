4b2f9421619af5237675b776bc8e667abdb6fcd1
TramNVN <duynhut0095@gmail.com>
2023-11-29 20:52:39 +0700

    add form

---
+++ QuanlySV/SrvConfirm.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace QuanlySV
+{
+    partial class SrvConfirm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label5 = new Label();
+            comboBox1 = new ComboBox();
+            label6 = new Label();
+            dateTimePicker2 = new DateTimePicker();
+            label7 = new Label();
+            textBox3 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Bottom;
+            dataGridView1.Location = new Point(0, 257);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(1023, 281);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Script MT Bold", 24F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(192, 0, 0);
+            label1.Location = new Point(359, 4);
+            label1.Name = "label1";
+            label1.Size = new Size(404, 48);
+            label1.TabIndex = 3;
+            label1.Text = "Xác Nhận Các Dịch Vụ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Script MT Bold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(38, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 24);
+            label2.TabIndex = 4;
+            label2.Text = "Mã Dịch Vụ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(194, 69);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(249, 34);
+            textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Script MT Bold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(38, 141);
+            label3.Name = "label3";
+            label3.Size = new Size(148, 24);
+            label3.TabIndex = 4;
+            label3.Text = "Mã Số Sinh Viên";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(194, 128);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(249, 34);
+            textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Script MT Bold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(38, 206);
+            label4.Name = "label4";
+            label4.Size = new Size(136, 24);
+            label4.TabIndex = 4;
+            label4.Text = "Ngày Đăng Ký";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dateTimePicker1.Location = new Point(193, 195);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 34);
+            dateTimePicker1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Script MT Bold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(545, 79);
+            label5.Name = "label5";
+            label5.Size = new Size(93, 24);
+            label5.TabIndex = 4;
+            label5.Text = "Xác Nhận";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(700, 70);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(278, 28);
+            comboBox1.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Script MT Bold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(545, 202);
+            label6.Name = "label6";
+            label6.Size = new Size(146, 24);
+            label6.TabIndex = 4;
+            label6.Text = "Ngày Xác Nhận";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dateTimePicker2.Location = new Point(700, 192);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(278, 34);
+            dateTimePicker2.TabIndex = 6;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Script MT Bold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(545, 138);
+            label7.Name = "label7";
+            label7.Size = new Size(74, 24);
+            label7.TabIndex = 4;
+            label7.Text = "Ghi Chú";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(700, 125);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(278, 34);
+            textBox3.TabIndex = 5;
+            // 
+            // SrvConfirm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1023, 538);
+            Controls.Add(comboBox1);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label7);
+            Controls.Add(label5);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "SrvConfirm";
+            Text = "SrvConfirm";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private DateTimePicker dateTimePicker1;
+        private Label label5;
+        private ComboBox comboBox1;
+        private Label label6;
+        private DateTimePicker dateTimePicker2;
+        private Label label7;
+        private TextBox textBox3;
+    }
+}
