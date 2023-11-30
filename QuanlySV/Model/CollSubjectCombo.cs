24cb17321ea0a47f12756d85522095aa2654f31c
Admin01 <duy.nguyen@thlone.vn>
2023-11-30 13:18:20 +0700

    ..

---
+++ QuanlySV/Model/CollSubjectCombo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanlySV.Model
+{
+    internal class CollSubjectCombo
+    {
+        public string SubjectId { get; set; }
+        public string SubjectName { get; set; }
+    }
+}
