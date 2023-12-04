5c70a727bf2b76f361be046015111a380434b562
TramNVN <duynhut0095@gmail.com>
2023-12-04 10:08:52 +0700

    commit cd

--- QuanlySV/obj/Debug/net6.0-windows/QuanlySV.AssemblyInfo.cs
+++ QuanlySV/obj/Debug/net6.0-windows/QuanlySV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanlySV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca17a6fc2565ae18e2efb7349b0cce87703073f")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanlySV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanlySV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
